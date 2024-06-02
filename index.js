cfd78826787efec4882f9e3f2d2264c8f391f2fc
Sbjn <78596823+Sbjn@users.noreply.github.com>
2024-06-02 23:16:45 +0900

    Update index.js "Cors test"

--- index.js
+++ index.js
@@ -7,10 +7,22 @@ const router = require("./routes")
 
 
 const app = express()
-app.use(cors({
-    origin : process.env.FRONEND_URL,
-    credentials: true
-}))
+app.use((req, res, next) => {
+  const allowedOrigins = [process.env.FRONEND_URL, 'http://localhost:8020', 'http://127.0.0.1:9000', 'http://localhost:9000'];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+       res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  //res.header('Access-Control-Allow-Origin', process.env.FRONEND_URL);
+  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', true);
+  return next();
+});
+// app.use(cors({
+//     origin : process.env.FRONEND_URL,
+//     credentials: true
+// }))
 app.use(express.json())
 app.use(cookieParser())
 
