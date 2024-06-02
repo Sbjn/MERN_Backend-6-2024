ff8825089d71b54e4475fe1dbcbc938f32677ddd
Sbjn <78596823+Sbjn@users.noreply.github.com>
2024-06-02 23:21:45 +0900

    Update index.js

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ const router = require("./routes")
 
 const app = express()
 app.use((req, res, next) => {
-  const allowedOrigins = [process.env.FRONEND_URL, 'http://localhost:8020', 'http://127.0.0.1:9000', 'http://localhost:9000'];
+  const allowedOrigins = [process.env.FRONEND_URL, 'https://sbjn-e-commerce-mern-2024-fi64n3mol-sbjns-projects.vercel.app', 'https://sbjn-e-commerce-mern-2024-fi64n3mol-sbjns-projects.vercel.app/', 'http://localhost:9000'];
   const origin = req.headers.origin;
   if (allowedOrigins.includes(origin)) {
-       res.setHeader('Access-Control-Allow-Origin', origin);
+       res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
   }
   //res.header('Access-Control-Allow-Origin', process.env.FRONEND_URL);
   res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
